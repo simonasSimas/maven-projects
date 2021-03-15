6aec9482a55a9c0bddb446e63f76f564567acf92
simonasSimas <sasimas198@gmail.com>
2021-03-15 11:52:08 +0200

    First generic exc fully done

--- src/main/java/eu/academy/test/App.java
+++ src/main/java/eu/academy/test/App.java
@@ -1,11 +1,24 @@
 package eu.academy.test;
 
+import java.lang.reflect.Array;
+
 /**
  * Hello world!
  * <<<<<<< HEAD
  */
 public class App {
     public static void main(String[] args) {
+        Integer[] numbs = {1,2,3};
+        String[] words = {"Hello", "World"};
 
+        App app = new App();
+        app.arrayPrint(numbs);
+        app.arrayPrint(words);
+
+    }
+    public <T> void arrayPrint(T[] array){
+        for (T element : array){
+            System.out.println(element.toString());
+        }
     }
 }
