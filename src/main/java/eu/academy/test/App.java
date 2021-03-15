0710f77d2af71ef1876e8bf4835b742fc339aeb8
simonasSimas <sasimas198@gmail.com>
2021-03-15 18:21:20 +0200

    Cleaned imports

--- src/main/java/eu/academy/test/App.java
+++ src/main/java/eu/academy/test/App.java
@@ -1,17 +1,11 @@
 package eu.academy.test;
 
-
-import eu.academy.test.service.SuperPair;
-
 /**
  * Hello world!
  * <<<<<<< HEAD
  */
 public class App {
     public static void main(String[] args) {
-        SuperPair<String, Double, Number> superPair1 = new SuperPair<>("a", 1.2, 10);
-        SuperPair<String, String, Number> superPair2 = new SuperPair<>("a", "b", 10);
-        SuperPair<String, String, Number> superPair3 = new SuperPair<>("a", "b", "10");
 
     }
 }
