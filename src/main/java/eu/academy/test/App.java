4b7bf8017317f52774d86e27e17cfb8009e0d25b
simonasSimas <sasimas198@gmail.com>
2021-03-18 18:23:09 +0200

    Cleared main from trash

--- src/main/java/eu/academy/test/App.java
+++ src/main/java/eu/academy/test/App.java
@@ -1,26 +1,10 @@
 package eu.academy.test;
 
 
-import eu.academy.test.service.PersonManager;
-
-import java.util.List;
-
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        PersonManager pm = new PersonManager();
-        pm.createPersonObjectFromPeopleDataLine();
-        pm.addSentAndReceivedAmountsToObject(pm.refractorFileToStringList(), pm.getMap());
-        System.out.println(pm.getMap().get(3297));
-
-//        Box box = new Box();
-//        box.set(67);
-//        Integer sveikasSk = (Integer) box.get();
-//
-//        box.set("kazkas");
-//    }
     }
 }
